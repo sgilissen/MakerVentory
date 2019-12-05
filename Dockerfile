026d49eaa790f2774ece44a151159638e657707f
Gilissen Steve <steve@pulseconsulting.be>
2019-12-05 17:08:42 +0100

    Added ENV for LDAP. Started on groups.

--- Dockerfile
+++ Dockerfile
@@ -11,4 +11,10 @@ ADD . /app
 EXPOSE 8000
 WORKDIR /app
 ENV PYTHONUNBUFFERED=1
+ENV LDAP_SERVER_URI="ldap://10.10.20.16:389"
+ENV LDAP_USER_DN_TEMPLATE="uid=%(user)s,cn=users,dc=pulselogic,dc=lan"
+ENV LDAP_GROUP_IS_STAFF='cn=mv_isstaff,cn=groups,dc=pulselogic,dc=lan"
+ENV LDAP_GROUP_IS_SUPERUSER='cn=mv_superuser,cn=groups,dc=pulselogic,dc=lan"
+ENV LDAP_GROUP_CAN_EDIT='cn=mv_canedit,cn=groups,dc=pulselogic,dc=lan"
+ENV LDAP_GROUP_READONLY='cn=mv_readonly,cn=groups,dc=pulselogic,dc=lan"
 CMD python manage.py runserver 0.0.0.0:8000
--- makerventory/settings.py
+++ makerventory/settings.py
@@ -108,10 +108,15 @@ AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
 ]
 
-AUTH_LDAP_SERVER_URI = "ldap://10.10.20.16:389"
-#AUTH_LDAP_USER_DN_TEMPLATE = "uid=%(user)s,cn=users,dc=lan,dc=pulselogic"
-AUTH_LDAP_USER_DN_TEMPLATE = "uid=%(user)s,cn=users,dc=pulselogic,dc=lan"
+AUTH_LDAP_SERVER_URI = os.environ['LDAP_SERVER_URI']
+AUTH_LDAP_USER_DN_TEMPLATE = os.environ['LDAP_USER_DN_TEMPLATE']
 AUTH_LDAP_USER_ATTR_MAP = {"first_name": "givenName", "last_name": "sn", "email": "mail"}
+
+AUTH_LDAP_USER_FLAGS_BY_GROUP = {
+   "is_staff":     os.environ['LDAP_GROUP_IS_STAFF'],
+   "is_superuser": os.environ['LDAP_GROUP_IS_STAFF'],
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
